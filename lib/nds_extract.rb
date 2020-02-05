eb49fffcb9ad83d03e60725182e6d3be2acc733f
Josh Couper <joshcouper@gmail.com>
2020-02-05 20:23:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,7 +26,7 @@ def directors_totals(nds)
   results = {}
   
   while nds.length > dir_counter do 
-    results[nds][counter][:name] = gross_for_director(nds[counter])
+    results[nds][dir_counter][:name] = gross_for_director(nds[dir_counter])
   dir_counter += 1 
   end
   results
